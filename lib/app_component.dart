82580965a2792f9dcaeda5deb4c46d99e8fb6bdc
Neven Miculinic <neven.miculinic@gmail.com>
2020-04-20 12:22:43 +0200

    finished tutorial 2

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,21 @@
 import 'package:angular/angular.dart';
+import 'package:angular_app/src/mock_heroes.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'src/hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  templateUrl: 'app_component.html',
+  styleUrls: [
+    'app_component.css',
+  ],
+  directives: [formDirectives, coreDirectives],
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour of heroes';
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = hero;
 }
